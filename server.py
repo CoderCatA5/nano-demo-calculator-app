e6284a78cf045adf8880a4b32d0b4fd4549221da
AYUSH SINGH <theayushsingh72@gmail.com>
2023-08-23 13:24:11 +0000

    Added endpoints: add subtract ang greeting

--- server.py
+++ server.py
@@ -1,19 +1,38 @@
-from flask import Flask
+from flask import jsoinify,Flask
+import request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    #respond with code 200 and concent:'hello world'
+    data={
+        'Code':200,
+        'Content':'Hello world'
+    }
+    return jsonify(data) 
+
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    incoming=request.json()
+
+    data={
+        'Status code':200,
+        'Content':incoming['first']+incoming['second']
+    }
+    return jsonify(data)
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    incoming=request.json()
+
+    data={
+        'Status code':200,
+        'Content':incoming['first']-incoming['second']
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
